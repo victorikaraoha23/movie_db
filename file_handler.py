669936d6d55a83fca21052e0eb2519aedd374743
victorikaraoha23 <victorikaraoha23@gmail.com>
2026-05-15 14:09:29 +0100

    revert tested sucessfully

--- file_handler.py
+++ file_handler.py
@@ -4,24 +4,15 @@ from collection import MovieCollection
 
 
 def save_collection(collection,filename):
-    try:
-     with open(filename,"w") as f:
+    with open(filename,"w") as f:
         for movie in collection.list_all():
             json.dump(movie.to_dict(),f)
             f.write("\n")
-    except FileNotFoundError:
-        pass
-        
         
     
 
 
 def load_collections(file_name):
-    try:
-     with open(file_name,):
-          collection_list=json.load(file_name)
-          for movies in collection_list:
-            loaded_movies=movies.from_dict()
-         return loaded_movies
-    except FileNotFoundError:
-            pass
+    with open(file_name,):
+        collection_list=json.load(file_name)
+        return collection_list
