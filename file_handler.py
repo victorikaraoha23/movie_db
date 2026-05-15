4b4c667e23149eafbdd9ab3e92c6ba7867cfac62
victorikaraoha23 <victorikaraoha23@gmail.com>
2026-05-15 13:19:09 +0100

    update the file_handler to include json parsing

--- file_handler.py
+++ file_handler.py
@@ -4,15 +4,24 @@ from collection import MovieCollection
 
 
 def save_collection(collection,filename):
-    with open(filename,"w") as f:
+    try:
+     with open(filename,"w") as f:
         for movie in collection.list_all():
             json.dump(movie.to_dict(),f)
             f.write("\n")
+    except FileNotFoundError:
+        pass
+        
         
     
 
 
 def load_collections(file_name):
-    with open(file_name,):
-        collection_list=json.load(file_name)
-        return collection_list
+    try:
+     with open(file_name,):
+          collection_list=json.load(file_name)
+          for movies in collection_list:
+            loaded_movies=movies.from_dict()
+         return loaded_movies
+    except FileNotFoundError:
+            pass
